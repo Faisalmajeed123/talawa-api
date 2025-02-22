8dd34c3180d0e7fb6ef4c86559e051a634a52b57
faisal majeed <faisalmajeedpak2@gmail.com>
2025-02-22 22:03:24 +0500

    changes

--- src/graphql/types/Fund/updater.ts
+++ src/graphql/types/Fund/updater.ts
@@ -1,6 +1,5 @@
 import { User } from "~/src/graphql/types/User/User";
 import { TalawaGraphQLError } from "~/src/utilities/TalawaGraphQLError";
-import type { GraphQLContext } from "../../context";
 import { Fund } from "./Fund";
 import { GraphQLContext } from "../../context";
 
@@ -86,87 +85,7 @@ const resolveUpdater = async (
   return existingUser;
 };
 
-const authenticateUser = async (ctx: GraphQLContext) => {
-	if (!ctx.currentClient.isAuthenticated) {
-		throw new TalawaGraphQLError({
-			extensions: { code: "unauthenticated" },
-		});
-	}
-
-	const currentUserId = ctx.currentClient.user.id;
-
-	const currentUser = await ctx.drizzleClient.query.usersTable.findFirst({
-		with: {
-			organizationMembershipsWhereMember: {
-				columns: { role: true },
-			},
-		},
-		where: (fields, operators) => operators.eq(fields.id, currentUserId),
-	});
-
-	if (currentUser === undefined) {
-		throw new TalawaGraphQLError({
-			extensions: {
-				code: "unauthenticated",
-			},
-		});
-	}
-
-	return currentUser;
-};
-
-const resolveUpdater = async (
-	parent: Fund,
-	_: unknown,
-	ctx: GraphQLContext,
-) => {
-	const currentUser = await authenticateUser(ctx);
-	const currentUserId = ctx.currentClient.user?.id;
-
-	const currentUserOrganizationMembership =
-		currentUser.organizationMembershipsWhereMember?.[0];
-
-	if (
-		currentUser.role !== "administrator" &&
-		(currentUserOrganizationMembership === undefined ||
-			currentUserOrganizationMembership.role !== "administrator")
-	) {
-		throw new TalawaGraphQLError({
-			extensions: {
-				code: "unauthorized_action",
-			},
-		});
-	}
-
-	if (parent.updaterId === null) {
-		return null;
-	}
-	if (parent.updaterId === currentUserId) {
-		return currentUser;
-	}
-
-	const updaterId = parent.updaterId;
-
-	const existingUser = await ctx.drizzleClient.query.usersTable.findFirst({
-		where: (fields, operators) => operators.eq(fields.id, updaterId),
-	});
-
-	if (existingUser === undefined) {
-		ctx.log.error(
-			"Postgres select operation returned an empty array for a fund's updater id that isn't null.",
-		);
-		throw new TalawaGraphQLError({
-			extensions: {
-				code: "unexpected",
-			},
-		});
-	}
-
-	return existingUser;
-};
-
 Fund.implement({
-<<<<<<< HEAD
   fields: (t) => ({
     updater: t.field({
       description: "User who last updated the fund.",
@@ -174,15 +93,6 @@ Fund.implement({
       type: User,
     }),
   }),
-=======
-	fields: (t) => ({
-		updater: t.field({
-			description: "User who last updated the fund.",
-			resolve: resolveUpdater,
-			type: User,
-		}),
-	}),
->>>>>>> upstream/develop-postgres
 });
 
 export { resolveUpdater };
